1cdfe463b88bec62d830696248baf8fd9e524099
sagarwl4 <sagarwl4@32f9b8dd-1df5-4980-91ca-96f565cb5c3b>
2013-01-29 22:55:16 +0000

    dis 3

--- lab3/hello-world.c
+++ lab3/hello-world.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
+ 
 #define MESSAGE "Hello, x86 world!"
-
+ 
 int main()
 {
-	puts (MESSAGE);
-	return 0;
+    puts(MESSAGE);
+    return 0;
 }
-
-gcc -E -o hello-world.i hello-world.c
-
-/*The preprocessor expands preprocessor directives, which is any line that starts with a "#". These are processed before your code is compiled.*/
