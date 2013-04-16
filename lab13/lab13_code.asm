907208e875016e02d1b0307d656c1a71263a5017
sagarwl4 <sagarwl4@32f9b8dd-1df5-4980-91ca-96f565cb5c3b>
2013-04-16 22:15:15 +0000

    lab 13 completed

--- lab13/lab13_code.asm
+++ lab13/lab13_code.asm
@@ -11,12 +11,12 @@
 ; R4 contains counter for inner loop
 ; R5 contains current sum
 
-            x3000
 
+.ORIG       x3000					  ;specifies where the program will be stored	
             LD    R1,INPUT            ; R1 contains input number
-            LD    R2,x3010            ; R2 contains -1
+            LD    R2,INPUT1           ; R2 contains -1
             ADD   R3,R1,R2            ; R3 contains input number -1
-            ADD   R3,,R3,R2           ; R3 contains input number -2
+            ADD   R3,R3,R2            ; R3 contains input number -2
                                       ;   (initializes outer count)
 OUTERLOOP   ADD   R4,R0,R3            ; Copy outer count into inner count
 
@@ -27,16 +27,18 @@ INNERLOOP   ADD   R5,R5,R1            ; Increment sum
             BRzp  INNERLOOP           ; Branch to inner loop if inner count
                                       ;   is positive or zero
             ADD   R1,R0,R5            ; R1 now contains sum result from inner loop
-            LD    R5,x300F            ; Clear R5 (previous sum) to 0
+            LD    R5,INPUT2           ; Clear R5 (previous sum) to 0
             ADD   R3,R3,R2            ; Decrement outer count
-            BRpz  OUTERLOOP           ; Branch to outer loop if outer count
+            BRzp  OUTERLOOP           ; Branch to outer loop if outer count
                                       ;   is positive or zero
 
-            ST   R1,x3011             ; This address contains X!          
-            TRAP x25
-
+            ST   R1,INPUT3             ; This address contains X!          
+            TRAP x25				  ; halt
+            
 INPUT      .FILL  x0005               ; Input for X!, in this case X = 5
-           .FILL  x0000
-           .FILL  xFFFF               ; 2's complement of 1 (i.e. -1)
-           .FILL  x0000               ; At program completion, the result is
+INPUT2     .FILL  x0000
+INPUT1     .FILL  xFFFF               ; 2's complement of 1 (i.e. -1)
+INPUT3     .FILL  x0000               ; At program completion, the result is
                                       ;   stored here
+                                      
+           .END                           
